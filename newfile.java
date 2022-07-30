9f205b055f26894951a59bdb82da09565dd8df6c
Nicholas Dobbin <96012796+DobbsZerker@users.noreply.github.com>
2022-07-30 12:41:29 -0230

    updated newfile

--- newfile.java
+++ newfile.java
@@ -1,7 +1,9 @@
 public class newfile {
     private String something;
+    private String name;
 
-    public newfile(String something){
+    public newfile(String something, String name){
         this.something = something;
+        this.name = name
     }
 }
